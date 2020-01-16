42ca6f50fc63c6430ed85754c98ea9a2dec994b4
Per Buer <per.buer@gmail.com>
2020-01-16 22:17:56 +0100

    ...

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "mgos.h"
+
+#include "mgos_ro_vars.h"
+#include "mgos_shadow.h"
+#include "mgos_app.h"
+#include "mgos_system.h"
+#include "mgos_timers.h"
+
+
+#include "mgos_sys_config.h"
+
+#include "mgos_i2c.h"
+#include "mgos_bme280.h"
+
+#include <frozen.h>
+
+
